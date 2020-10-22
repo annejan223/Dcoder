1e9d6d32d4134f1f767d1936e28cad9b03c147c2
anne <annebraaksma@hotmail.nl>
2020-10-22 21:45:52 +0200

    Update Js.js

--- Js.js
+++ Js.js
@@ -1,44 +1,66 @@
-let input= document.getElementById("input");
-let result = document.getElementById("resultaten");
+let gebruikers= [
+  {
+    email: "Bonita.Lang@hotmail.com",
+    name: "Lamont Bruen"
+  },
+  {
+    email: "Elody_Kuhic@gmail.com",
+    name: "Hipolito McGlynn"
+  },
+  {
+    email: "Brock_Baumbach1@hotmail.com",
+    name: "Darion Kemmer"
+  },
+  {
+    email: "Candido_Braun@hotmail.com",
+    name: "Doyle Predovic"
+  },
+  {
+    email: "Loma67@hotmail.com",
+    name: "Hulda Kuhic"
+  },
 
+  {
+    email: "Loma67@hotmail.com",
+    name: "anne braaksma1"
+  },
 
-input.addEventListener('input', (key) => {
-key = input.value.toLowerCase();
-
-const data = gebruikers.filter(gebruiker =>{   
-  //return gebruiker.name.toLowerCase().includes(key);
-  const filtered = gebruiker.name.toLowerCase().includes(key);
-
-  if(filtered){
-    return gebruiker; 
-  }else{
-   console.log(!1); 
-   removeappend();
-   return false;
+  {
+    email: "anne@hotmail.com",
+    name: "Anne Braaksma"
   }
+  ];
 
+let input= document.getElementById("input");
+//let result = document.getElementById("resultaten");
+const resultatenLijst = document.getElementById("resultaten");
+
+
+input.addEventListener('input', (event) => {
+
+  const key = input.value.toLowerCase();
+  console.log(key);
+  resultatenLijst.innerHTML = "";
+  const spanElem = document.createElement('span');
+
+
+  const data = gebruikers.filter(
+      gebruiker => gebruiker.name.toLowerCase().includes(key)
+    );
+
+
+
+    data.forEach(gebruiker =>{
+      var boldString = (s, b) => s.replace(RegExp(b, 'gi'), `<b>${key}</b>`); 
+      const liElem = document.createElement('li');
+      liElem.innerHTML = boldString(gebruiker.name, key);
+      resultatenLijst.append(liElem);
+    });
+
+  if(!data.length || key == ""){
+     resultatenLijst.innerHTML = "<li>Geen resultaten gevonden</li>";
+  }
 });
-
-
-const GetNames = data.forEach( gebruikers =>{
-console.log(gebruikers.name); 
-
-const gebruikersnamen = document.createElement('li');
-gebruikersnamen.classList.add('li');
-gebruikersnamen.innerHTML = gebruikers.name;
-document.getElementById("resultaten").append(gebruikersnamen);
-});
-
-function removeappend(){
- .classList.removeChild('li');
-}
-
-
-
-
-});
-
-
 
 
 
